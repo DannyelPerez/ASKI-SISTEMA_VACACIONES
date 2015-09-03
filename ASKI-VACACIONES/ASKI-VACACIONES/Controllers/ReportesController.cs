a42fa2d4dfda95484233f1a36802ad66097188e3
Josselyn Medina <josselynmedina@unitec.edu>
2015-09-03 02:46:04 -0600

    Reporte usuario
    falta ayuda, font y validaciones

---
+++ ASKI-VACACIONES/ASKI-VACACIONES/Controllers/ReportesController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASKI_VACACIONES.Controllers
+{
+    public class ReportesController : Controller
+    {
+        // GET: Reportes
+        public ActionResult Usuarios() {return View();}
+    }
+}
