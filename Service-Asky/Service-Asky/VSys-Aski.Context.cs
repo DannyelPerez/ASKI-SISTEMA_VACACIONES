8c09293c8d9c3aa33819350a50ddd7d3a455e44c
Daniel Perez <danielperez@unitec.edu>
2015-09-14 17:58:51 -0600

    Fixed Errors

--- Service-Asky/Service-Asky/VSys-Aski.Context.cs
+++ Service-Asky/Service-Asky/VSys-Aski.Context.cs
@@ -12,9 +12,10 @@ namespace Service_Asky
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Infrastructure;
+    
     public partial class vsystem_askiEntities : DbContext
     {
-             public vsystem_askiEntities()
+        public vsystem_askiEntities()
             : base("name=vsystem_askiEntities")
         {
         }
