e997a475f6bbef2b2d93785732f7d2b76c55b6d5
Josselyn Medina <josselynmedina@unitec.edu>
2015-09-25 15:07:44 -0600

    cambios

--- ASKI-VACACIONES/ASKI-VACACIONES/Models/SolicitudesModel.cs
+++ ASKI-VACACIONES/ASKI-VACACIONES/Models/SolicitudesModel.cs
@@ -7,8 +7,20 @@ namespace ASKI_VACACIONES.Models
 {
     public class SolicitudesModel
     {
-        public string Ciudad { get; set; }
-        public DateTime fechaCreacionSolicitud { get; set; }
+        public UsuariosModel usuarios { get; set; }
+        public int talento_humano { get; set; }
+
+        public int id { get; set; }
+        public string idvacaciones { get; set; }
+        public DateTime Fechainicio { get; set; }
+        public DateTime Fechafin { get; set; }
+
+        public DateTime year { get; set; }
+        public int dias_solicitados { get; set; }
+        public DateTime fecha_solicitud { get; set; }
+        public DateTime fecha_aprobacion { get; set; }
+        public int estatudid { get; set; }
+
         
 
 
